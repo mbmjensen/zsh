a01725c66147f994b9d3fda4bec62a33d3eb0af7
Michael Jensen <michaelbmjensen@gmail.com>
2020-11-11 23:31:30 -0800

    Update FZF colors
    This also fixes fzf configuration. The way this is currently set up is
    brittle because the paths used to configure fzf have the version hard
    coded. It would be better to make fzf a submodule to my config files.

--- fzf.zsh
+++ fzf.zsh
@@ -2,24 +2,24 @@
 export FZF_DEFAULT_COMMAND='fd --type f --type d --hidden --follow --exclude .git'
 
 # Fix the colors that fzf uses to match colorscheme
-export FZF_DEFAULT_OPTS='
- --color=fg:#5C6773,bg:#FAFAFA,hl:#ABB0B6
- --color=bg+:#F3F3F3,hl+:#FF7733
- --color=info:#E6B673,prompt:#FF7733,pointer:#FF7733
- --color=marker:#E7C547,spinner:#FF7733,header:#ABB0B6'
+export FZF_COLORS='
+  --color=fg:#9c9c9b,bg:#eeeeed,hl:#d75e00
+  --color=fg+:#4c4c4b,gutter:#eeeeed,bg+:#e4e4e4,hl+:#d75e00
+  --color=info:#9c9c9b,prompt:#8959a8,pointer:#d6225e
+  --color=marker:#4271ad,spinner:#4271ad,header:#4271ad'
+
+export FZF_DEFAULT_OPTS=$FZF_COLORS
 
 # Use the same default command for Ctrl-T
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
 # Setup
-if [[ ! "$PATH" == */usr/local/Cellar/fzf/0.23.0/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/usr/local/Cellar/fzf/0.23.0/bin"
+if [[ ! "$PATH" == */usr/local/Cellar/fzf/0.23.1/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/usr/local/Cellar/fzf/0.23.1/bin"
 fi
 
 # Initialize auto-completion
-[[ $- == *i* ]] && source "/usr/local/Cellar/fzf/0.23.0/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "/usr/local/Cellar/fzf/0.23.1/shell/completion.zsh" 2> /dev/null
 
 # Initialize key bindings
-source "/usr/local/Cellar/fzf/0.23.0/shell/key-bindings.zsh"
-[[ -f $XDG_CONFIG_HOME/fzf/fzf.zsh ]] && source $XDG_CONFIG_HOME/fzf/fzf.zsh
-
+source "/usr/local/Cellar/fzf/0.23.1/shell/key-bindings.zsh"
