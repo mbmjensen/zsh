b5847b63376681f94ec5f733784938183bf9ed63
Michael Jensen <michaelbmjensen@gmail.com>
2020-11-11 23:25:46 -0800

    Use Ctrl-J to open the line in $EDITOR
    Previously this was triggered using ^<SPACE>, but this mapping is now
    used to add a space without expanding the previous term.

--- options.zsh
+++ options.zsh
@@ -1,13 +1,13 @@
 # Reduce latency when pressing <Esc>
 export KEYTIMEOUT=1
 
-# Use emacs style commands inline.
+# Use emacs style commands inline
 bindkey -e
 
-# On escape, edit the current command in $EDITOR
+# Use CTRL-J to edit the current command in $EDITOR
 autoload -U edit-command-line
 zle -N edit-command-line
-bindkey '^ ' edit-command-line
+bindkey '^j' edit-command-line
 
 # Follow sym links
 setopt chase_links
