110954920c2548181e152392de423a66a9c38736
Michael Jensen <michaelbmjensen@gmail.com>
2020-11-12 17:07:21 -0800

    Enable auto_pushd and pushd_ignore_dups
    Together auto_pushd and pushd_ignore_dups make cd save the old directory
    to the dirstack, while ignoring duplicate entries.

--- options.zsh
+++ options.zsh
@@ -9,6 +9,10 @@ autoload -U edit-command-line
 zle -N edit-command-line
 bindkey '^j' edit-command-line
 
+# Make cd push the old directory onto the directory stack (ignoring duplicates)
+setopt auto_pushd
+setopt pushd_ignore_dups
+
 # Follow sym links
 setopt chase_links
 
